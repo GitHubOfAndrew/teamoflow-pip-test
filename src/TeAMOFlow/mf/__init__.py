b7b14a48c89fed760cb1f655d7b612dbb535a1eb
GitHubOfAndrew <Changjyandrew@gmail.com>
2022-07-13 09:59:00 -0700

    Don't exclude file from bdist, just don't import it in __init__.py

--- src/TeAMOFlow/mf/__init__.py
+++ src/TeAMOFlow/mf/__init__.py
@@ -5,6 +5,5 @@ from . import embedding_graphs
 from . import initializer_graphs
 from . import input_utils
 from . import utils
-from . import benchmarking_ML
 
-__all__ = ['matrix_factorization', 'loss_graphs', 'predict_graphs', 'embedding_graphs', 'initializer_graphs', 'input_utils', 'utils', 'benchmarking_ML']
+__all__ = ['matrix_factorization', 'loss_graphs', 'predict_graphs', 'embedding_graphs', 'initializer_graphs', 'input_utils', 'utils']
